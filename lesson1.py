9921457995c06375e8bf9d59c3f00c3a9c2817c9
Arhan Ansari <arhanansari2009@gmail.com>
2024-09-15 22:58:04 +0530

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,30 @@
+#Lesson No.1 Print Hello World!
+print('Hello World!')
+print("Hello World!")
+
+#Printing multiple lines using print() function
+print("""Hi, I am Ahmad,
+      I am going to learn Python from my brother for free.
+      VS Code is the free code editor for Windows.""") #This is the RIGHT Syntax to print multiple lines
+# print("Hi, I am Ahmad
+#       I am going to learn Python from my brother for free.
+#       VS Code is the free code editor for Windows.") #This is the WRONG Syntax
+
+#Practice Time
+
+#Solve the following problems
+
+#Problem 1: Write your name in console using print() function
+#Solution:
+print("my name is ahmad")
+
+#Problem 2: Write this multi line statement using print() function.
+#Statement: 
+"""What is Module?
+A module is a file containing code written by somebody else (usually) which can be 
+imported and used in our programs."""
+
+#Solution:
+print("""What is Module?
+A module is a file containing code written by somebody else (usually) which can be 
+imported and used in our programs.""")
---
+++ lesson2.py
@@ -0,0 +1,40 @@
+#Lesson No.2: Inputs & Variables
+
+#Inputs in Python
+#input() #Wait untill you type
+
+# input("Name: ")
+# input("Enter the number: ")
+
+#Variables
+#Variable is the container in which we store some information orr Data!
+
+# myVar = 56 #Integer Variable
+# myFloat = 5.36 #Float Variable
+# myString = "Hello World" #String Variable
+# myChar = "abc" #Character Variable
+
+#Storing User Input
+# myName = input("Name: ")
+# print(myName) #Printing Variable
+
+#Overwriting Variables
+# myName = "Arhan"
+# print(myName)
+
+# userName = input("Enter your name: ")
+# print(userName, "is your name")
+
+#Solve the following problems
+
+#Problem No.1: Take the name as the input from the user
+#Solution:
+input("Name: ")
+
+#Problem No.2: Now store the input that you get in problem no.1 using varibales
+#Solution:
+myName = input("Name: ")
+
+#Problem No.3: Now write the sentence. (Sentemce should look like: userInput is your name)
+#Solution:
+print(myName, "is your name")
